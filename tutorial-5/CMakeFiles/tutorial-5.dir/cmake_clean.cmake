234cfe19d47db9410227e74906398931b0720d4f
Bruce Thomas <rheotaxis@gmail.com>
2023-01-06 23:19:44 -0800

    trying to build C++ demo
     code fails at build up

---
+++ tutorial-5/CMakeFiles/tutorial-5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial-5.dir/main.cpp.obj"
+  "CMakeFiles/tutorial-5.dir/main.cpp.obj.d"
+  "libtutorial-5.dll.a"
+  "tutorial-5.exe"
+  "tutorial-5.exe.manifest"
+  "tutorial-5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorial-5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
