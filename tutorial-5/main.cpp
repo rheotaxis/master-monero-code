3d28cb111bd51e23a18575f184b9b86eac16af69
Bruce Thomas <rheotaxis@gmail.com>
2023-03-14 23:38:30 -0700

    trying makefile

--- tutorial-5/main.cpp
+++ tutorial-5/main.cpp
@@ -1,10 +1,12 @@
 // main.cpp file for Tutorial 5 - Mastering Monero
 
+#include <iostream>
+/*
 #include "cryptonote_core/blockchain.h"
 #include "common/base58.h"
 #include "crypto/crypto-ops.h"
 #include "crypto/hash.h"
- 
+*/
 // Converts crypto::hash into crypto::secret_key or crypto::public_key
 /* template <typename T>
 T get_key_from_hash(crypto::hash & in_hash){
@@ -13,7 +15,9 @@ T get_key_from_hash(crypto::hash & in_hash){
 	return *key;
 }
  */
+
 int main(){
+    std::cout << "Hello World!";
 /*   	// Put here your private spendable key!
 	std::string str_spend_key = "f8f2fba1da00643bbf11ffec355a808d2d8ca4e4de14a10476e116abd8dd7f02";
 	cryptonote::network_type nettype = cryptonote::MAINNET;	
